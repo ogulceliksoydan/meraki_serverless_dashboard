85f3bfcf2a5fb6e8784d3f0dc6365755c64b2fe1
ogulceliksoydan <ogulceliksoydan@gmail.com>
2020-08-19 20:46:51 +0300

    applied display none

--- script2.js
+++ script2.js
@@ -14,18 +14,20 @@ function checkLogin(){
     var cognitoUser = userPool.getCurrentUser();
 
     if (cognitoUser == null) {
-        document.getElementById("signin").style = "visibility:visible";
+        document.getElementById("signin").style = "display:block";
     } else {
         getTemplates();
     }
 }
 
 function login(){
+    document.getElementById("loading").style = "display:block";
+    
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     var authenticationData = {Username: username, Password: password};
 
-    console.log("Username:",username , "Password:",password);
+    console.log("Username:", username, "Password:", password);
 
     var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
 
@@ -39,14 +41,17 @@ function login(){
             localStorage.token = idToken;
             localStorage.user = username
             
-            document.getElementById("signin").style = "visibility:hidden";
-            checkLogin();
+            document.getElementById("signin").style = "display:none";
+            getTemplates();
+
             console.log("Authentication successful");
             //console.log("ID Token:", idToken);
             //console.log("Access Token:", accessToken);
         },
 
         onFailure: function(err) {
+            document.getElementById("loading").style = "display:none";
+
             console.log(JSON.stringify(err))
             alert(JSON.stringify(err.message))
         },
@@ -56,11 +61,15 @@ function login(){
 function logOut() {
     var cognitoUser = userPool.getCurrentUser();
     cognitoUser.signOut();
+
     window.location = './index2.html';
+    
     console.log("Logged out")
 }
 
 function getTemplates(){
+    document.getElementById("loading").style = "display:block";
+
     var request = new XMLHttpRequest();
     request.open('GET', url);
     request.setRequestHeader('Authorization', localStorage.getItem('token'));
@@ -73,6 +82,7 @@ function getTemplates(){
                 console.log("Complete data:", typeof this, this);
                 var data = JSON.parse(this.response);
                 console.log("Dropdown items:", typeof data, data);
+
                 data.forEach(item => {
                     var para = document.createElement("option");
                     var node = document.createTextNode(item.id + ":" + item.name);
@@ -83,7 +93,9 @@ function getTemplates(){
                 var msg = "Welcome " + localStorage.getItem('user');
                 document.getElementById("welcome").innerHTML = msg;
                 document.getElementById("welcome").style = "font-weight:bold";
-                document.getElementById("main").style = "visibility:visible";
+                document.getElementById("loading").style = "display:none";
+                document.getElementById("main").style = "display:block";
+
             } else {
                 console.log("Token may have expired. Logging out");
                 logOut();
@@ -94,16 +106,14 @@ function getTemplates(){
     request.send();
 }
 
-function post(){
+function postObject(){
     var netwname = document.getElementById("netname").value;
     var template = document.getElementById("anchor").value;
     var serials = document.getElementById("serials").value;
     var myobj = {"netname": netwname, "template": template.split(':')[0], "serials": serials.replace(/ /g, "").split('\n')};
-    console.log("Created object:", myobj);
-    createNetwork(myobj);
-}
 
-function createNetwork(postObj){
+    console.log("Created object:", myobj);
+    
     var request = new XMLHttpRequest();
     request.open('POST', url);
     request.setRequestHeader('Authorization', localStorage.getItem('token'));
@@ -116,7 +126,9 @@ function createNetwork(postObj){
                 console.log("Complete data:", typeof this, this)
                 var data = JSON.parse(this.response);
                 console.log("Message:", typeof data, data);
+
                 document.getElementById("message").innerHTML = this.response;
+
             } else {
                 console.log("Token may have expired. Logging out");
                 logOut();
@@ -124,7 +136,7 @@ function createNetwork(postObj){
         }
     }
     
-    //request.send(postObj);
-    request.send(JSON.stringify(postObj));
-    document.getElementById("message").innerHTML = "Loading..."
+    //request.send(myobj);
+    request.send(JSON.stringify(myobj));
+    document.getElementById("message").innerHTML = "Loading..."    
 }
